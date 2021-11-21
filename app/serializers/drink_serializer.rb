1ad1900d2da62f0612a6bed586ddba68baf87771
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-21 13:41:29 +0000

    Committing code before changing db

--- app/serializers/drink_serializer.rb
+++ app/serializers/drink_serializer.rb
@@ -1,6 +1,6 @@
 class DrinkSerializer < ActiveModel::Serializer
   attributes :name, :drink_type, :concentration, :manufacturer
-  # def manufacturer
+  # def manufacturers
   #   {manufacturer_name: self.object.manufacturer.name,
   #   manufacturer_year_founded: self.object.manufacturer.year_founded,
   #   manufacturer_country_founded: self.object.manufacturer.country_founded}
--- app/serializers/manufacturer_serializer.rb
+++ app/serializers/manufacturer_serializer.rb
@@ -1,3 +1,3 @@
 class ManufacturerSerializer < ActiveModel::Serializer
-  attributes :name, :year_founded, :country_founded
+  attributes :name, :year_founded, :country_founded, :drinks
 end
